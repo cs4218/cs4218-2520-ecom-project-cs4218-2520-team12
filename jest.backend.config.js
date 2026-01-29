40a9b959dd9bc50fabc5f3db859c4dd64fd9aac0
thony-ui <anthonyhermanto1@gmail.com>
2026-01-29 22:44:22 +0800

    reduce coverage to 95

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -13,8 +13,8 @@ export default {
   collectCoverageFrom: ["controllers/**"],
   coverageThreshold: {
     global: {
-      lines: 100,
-      functions: 100,
+      lines: 95,
+      functions: 95,
     },
   },
 };
