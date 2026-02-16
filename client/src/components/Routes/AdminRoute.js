a91e3287b492fba8ee2cbe1e707fc90cc74162d6
An Wei <wonganwei8@gmail.com>
2026-02-16 23:57:19 +0800

    Add unit tests for Routes

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -1,8 +1,8 @@
+import React from "react";
 import { useState,useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from 'axios';
-import { set } from "mongoose";
 import Spinner from "../Spinner";
 
 export default function AdminRoute(){
@@ -11,10 +11,14 @@ export default function AdminRoute(){
 
     useEffect(()=> {
         const authCheck = async() => {
-            const res = await axios.get("/api/v1/auth/admin-auth");
-            if(res.data.ok){
-                setOk(true);
-            } else {
+            try {
+                const res = await axios.get("/api/v1/auth/admin-auth");
+                if(res.data.ok){
+                    setOk(true);
+                } else {
+                    setOk(false);
+                }
+            } catch (error) {
                 setOk(false);
             }
         };
