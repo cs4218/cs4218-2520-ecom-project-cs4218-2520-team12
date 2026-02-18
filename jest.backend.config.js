26723edcf86bb8e98b60a48a73aa8d5a70e23c05
amoschee <amoscheetianee@gmail.com>
2026-02-18 20:42:37 +0800

    Refactor: move userModel tests to models directory and update Jest config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -6,11 +6,18 @@ module.exports = {
     testEnvironment: "node",
 
     // which test to run
-    testMatch: ["<rootDir>/controllers/__tests__/*.test.js"],
+    testMatch: [
+        "<rootDir>/controllers/__tests__/*.test.js",
+        "<rootDir>/models/__tests__/*.test.js",
+    ],
 
     // jest code coverage
     collectCoverage: true,
-    collectCoverageFrom: ["controllers/**", "!controllers/authController.js"],
+    collectCoverageFrom: [
+        "controllers/**",
+        "!controllers/authController.js",
+        "models/**",
+    ],
     coverageThreshold: {
         global: {
             lines: 95,
