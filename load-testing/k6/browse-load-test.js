50bd6cd5e48b1ce8e6150733a0054c2310a0e286
thony-ui <anthonyhermanto1@gmail.com>
2026-03-30 15:40:53 +0800

    ms3-load testing fixes

--- load-testing/k6/browse-load-test.js
+++ load-testing/k6/browse-load-test.js
@@ -11,7 +11,7 @@ export const options = {
         { duration: '30s', target: 0 },
     ],
     thresholds: {
-        http_req_duration: ['p(90)<500'], 
+        http_req_duration: ['p(90)<1500'], 
         http_req_failed: ['rate<0.01'],
     },
 };
