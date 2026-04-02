adb73f048cd54ad06e62a82d11c6d609235238bb
thony-ui <anthonyhermanto1@gmail.com>
2026-04-02 22:48:30 +0800

    fix load testing scripts

--- performance-testing/k6/load/search-load-test.js
+++ performance-testing/k6/load/search-load-test.js
@@ -5,7 +5,7 @@ import { check, sleep } from 'k6';
 export const options = {
     stages: [
         { duration: '30s', target: 200 },
-        { duration: '1m', target: 1000 }, // Faster ramp up for search
+        { duration: '1m', target: 1000 }, 
         { duration: '2m', target: 1000 },
         { duration: '30s', target: 0 },
     ],
